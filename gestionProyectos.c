cdb2ab9d29bf8ba49be03b5608bd59ab7ffda617
Sergio_Raya <sergiorayarobles@gmail.com>
2024-02-05 23:15:05 +0100

    Añadiendo cuerpo al main y creando estructuras

--- gestionProyectos.c
+++ gestionProyectos.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct Empleado {
     char nombreCompleto[50];
@@ -11,10 +13,25 @@ typedef struct Proyecto {
     char nombre[50];
     char descripcion[80];
     int id;
-    Empleado* listEmpl;
-};
+    Empleado listEmpl[5];
+} Proyecto;
 
 
 int main () {
+    Proyecto proyecto;
+
+    printf("Introduce el nombre del proyecto: ");
+    fgets(proyecto.nombre,sizeof(proyecto.nombre),stdin);
+    proyecto.nombre[strlen(proyecto.nombre) - 1] = '\0';
+    getchar();
+
+    printf("Introduce la descripción");
+    fgets(proyecto.descripcion,sizeof(proyecto.descripcion),stdin);
+    proyecto.descripcion[strlen(proyecto.descripcion) - 1] = '\0';
+    getchar();
+
+    printf("Introduce la id: ");
+    scanf("%d", proyecto.id);
+    getchar();
     
 }
