b2dbaa679248d70e9193a20258cc5790953195c4
Sergio_Raya <sergiorayarobles@gmail.com>
2024-02-07 16:20:47 +0100

    Funciones completadas

--- gestionProyectos.c
+++ gestionProyectos.c
@@ -16,7 +16,10 @@ typedef struct Proyecto {
     Empleado listEmpl[5];
 } Proyecto;
 
+void agregarEmpleadoAProyecto(Proyecto* proyecto, int empleado);
 void mostrarEquipoProyecto(Proyecto* proyecto);
+void mostrarDatosEmpleados(Proyecto* proyecto);
+void mostrarDatosProyecto(Proyecto* proyecto);
 
 int main () {
     Proyecto proyecto;
@@ -34,13 +37,43 @@ int main () {
     printf("Introduce la id: ");
     scanf("%d", proyecto.id);
     getchar();
-    
+
+    for (int i = 0; i < sizeof(proyecto.listEmpl)/sizeof(Empleado); i++) {
+        agregarEmpleadoAProyecto(&proyecto, i);
+    }
+
+    mostrarEquipoProyecto(&proyecto);
 }
 
-void agregarEmpleadoAProyecto() {
+void agregarEmpleadoAProyecto(Proyecto* proyecto, int empleado) {
 
+    printf("Introduce el nombre del empleado %d:", empleado + 1);
+    fgets(proyecto->listEmpl[empleado].nombreCompleto,sizeof(proyecto->listEmpl[empleado].nombreCompleto),stdin);
+    proyecto->listEmpl[empleado].nombreCompleto[strlen(proyecto->listEmpl[empleado].nombreCompleto) - 1] = '\0';
+    getchar();
+
+    printf("Introduce el numero de empleado: ");
+    scanf("%d", proyecto->listEmpl[empleado].numEmpl);
+
+    printf("Introduce el numero de empleado: ");
+    scanf("%lf", proyecto->listEmpl[empleado].salario);
 }
 
 void mostrarEquipoProyecto(Proyecto* proyecto) {
+    mostrarDatosProyecto(proyecto);
+    mostrarDatosEmpleados(proyecto);
+}
 
+void mostrarDatosProyecto(Proyecto* proyecto) {
+    printf("\nNombre del proyecto: %s", proyecto->nombre);
+    printf("\nDescripcion: %s", proyecto->descripcion);
+    printf("\nID: %d", proyecto->id);
+}
+
+void mostrarDatosEmpleados(Proyecto* proyecto) {
+    for (int i = 0; i < sizeof(proyecto->listEmpl)/sizeof(Empleado); i++) {
+        printf("\nNombre del empleado: %s", proyecto->listEmpl[i].nombreCompleto);
+        printf("\nNumero de empleado: %d", proyecto->listEmpl[i].numEmpl);
+        printf("\nID: %.2lf", proyecto->listEmpl[i].salario);
+    }
 }
