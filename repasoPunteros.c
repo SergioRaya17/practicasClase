63a7f0c7efa2164fb9f56eabf6eb8d21aeaa1e61
Sergio_Raya <sergiorayarobles@gmail.com>
2024-02-14 15:31:23 +0100

    Terminado

--- repasoPunteros.c
+++ repasoPunteros.c
@@ -13,4 +13,19 @@ int main () {
         printf("El valor es distinto.");
     }
 
+    //Incrementamos el valor desde el puntero.
+    (*ptrNum)++;
+    printf("\n\nValor del numero tras incrementar el puntero: %d.\n", num);
+
+    char mensaje[40] = "Este es el mesaje inicial. ;)";
+    char* ptrMensaje = mensaje;
+
+    printf("\n\nLa variable mensaje tiene almacenado: \"%s\"\n", ptrMensaje);
+
+    int numerosRacionales[5] = {1, 2, 3, 4, 5};
+    ptrNum = numerosRacionales;
+    for (int i = 0; i < 5; i++) {
+        printf("\n%d", ptrNum[i]);
+    }
+    
 }
