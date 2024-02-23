c1650055c1b00278c5e51dba7b3970d75d2820dc
Sergio_Raya <sergiorayarobles@gmail.com>
2024-02-23 11:13:05 +0100

    Creacion de funcion Crear Centro

--- Gestion de Centros/centros.c
+++ Gestion de Centros/centros.c
@@ -3,15 +3,35 @@
 #include "./estructuras.h"
 #include "./utils.h"
 
+void crearCentro (Centro centro);
+
 int main () {
     int numCentros;
-    printf("Cuantos centros vas a introducir: ");
+    printf("Introduce el número de centros: ");
     scanf("%d", &numCentros);
-    Centro centros [numCentros];
 
-    
-    Alumno alumno;
-    strcpy(alumno.datos.nombre, "Hola"); 
+    Centro centros[numCentros];
+    for (int i = 0; i < numCentros; i++) {
+        crearCentro(centros[i]);
+    }
+}
+
+void crearCentro (Centro centro) {
+    printf("Introduce el nombre del centro: ");
+    fgets(centro.nombre, sizeof(centro.nombre), stdin);
+    centro.nombre[strlen(centro.nombre) - 1] = '\0';
+    while (getchar() != '\n');
+
+    for (int i = 0; i < NUMERO_CICLOS; i++) {
+        printf("Dale un nombre al ciclo %d del %s\nIntroduce el nombre: ", i, centro.nombre);
+        fgets(centro.ciclos[i].nombre, sizeof(centro.ciclos[i].nombre), stdin);
+        centro.ciclos[i].nombre[strlen(centro.ciclos[i].nombre) - 1] = '\0';
+        while (getchar() != '\n');
+
+        
+    }
+
+
 }
 
 
